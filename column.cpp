5ed27471a06a07e84e1e281bf598146658e4e312
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2023-01-05 21:24:46 -0500

    format

--- column.cpp
+++ column.cpp
@@ -1,26 +1,26 @@
 
 #include "column.hpp"
 using namespace std;
-        /** 
-         * @brief Construct a new Column object
-         * 
-         * @param size Number of rows
-         * @param weights Weights of values
-         * @param values Values to be chosen from
-         */
-    Column::Column(int size, const vector<double>& weights, const vector<int>& values) : col(size)  {
-        
+/**
+ * @brief Construct a new Column object
+ *
+ * @param size Number of rows in column
+ * @param weights Weights of values
+ * @param values Values to be chosen from
+ */
+Column::Column(int size, const vector<double> &weights, const vector<int> &values) : col(size)
+{
+
     mt19937 rng(random_device{}());
     discrete_distribution<int> dist(weights.begin(), weights.end());
-    
-    generate(col.begin(), col.end(), [&] { return values[dist(rng)]; });
 
-    //no return here
-    
+    generate(col.begin(), col.end(), [&] { return values[dist(rng)]; });
 }
 
-void Column::print(){
-    for (auto &i : col){
+void Column::print()
+{
+    for (auto &i : col)
+    {
         cout << i << "\n";
     }
 }
