43c57933d0da47a776d43881d05bc8f2188d6b0b
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2023-01-05 19:47:18 -0500

    Co-authored-by: Michael Rivera <Mikerive@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <vector>
+
+
+
+
 
 int main(){
-std::cout << "Hi Mike";
+ std::cout << "Hi Mike\n";
+ std::vector<int> vec({2,3,4,5});
+ 
+ for (int &i : vec){
+    std::cout << i << "\n";
+ }
 }
+
+
+/*
+Column Class
+
+*/
+
+/*
+    SingleColumnCreation - input a 2d array and return a single column
+*/
